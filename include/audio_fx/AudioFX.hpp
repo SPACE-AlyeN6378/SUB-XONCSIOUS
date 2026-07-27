97ba81c8a360888c2f72895982d50262225cda18
SPACE-AlyeN6378 <alymooltazeem@gmail.com>
2026-07-26 21:51:57 -0700

    Audio FX abstraction class added

---
+++ include/audio_fx/AudioFX.hpp
@@ -0,0 +1,10 @@
+
+
+class AudioEffects {
+    /*
+    TODO: (Future consideration) Complete this abstract class, and extend other audio effects to this one
+    TODO: Add the class instance "Stereo" for stereo support for this DAW with *operator overloading*
+    */
+    
+
+};
